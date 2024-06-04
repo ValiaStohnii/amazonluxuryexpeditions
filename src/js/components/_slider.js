f8ba92e943b0260b060246bfc0f071f1f6bdf054
valia <v.stohnii@elogic.com.ua>
2024-06-04 18:59:41 +0300

    header and hero

--- src/js/components/_slider.js
+++ src/js/components/_slider.js
@@ -1,33 +1,12 @@
 import Swiper, { Navigation, Pagination, EffectFade, Scrollbar } from 'swiper';
 
-const swiperTeam = new Swiper('.crafting-slider', {
-  slidesPerView: 3,
+const swiperTeam = new Swiper('.about-slider', {
+  slidesPerView: 1,
   spaceBetween: 20,
   modules: [Navigation, Pagination],
-  pagination: {
-    clickable: true,
-    el: '.crafting .swiper-pagination',
-  },
-
-  draggable: true,
-  grabCursor: true,
   navigation: {
-    nextEl: '.crafting .btn--next',
-    prevEl: '.crafting .btn--prev',
-  },
-  breakpoints: {
-    992: {
-      slidesPerView: 3,
-    },
-    650: {
-      slidesPerView: 2,
-    },
-    611: {
-      slidesPerView: 1,
-    },
-    320: {
-      slidesPerView: 1,
-    }
+    nextEl: '.about .btn--next',
+    prevEl: '.about .btn--prev',
   },
 });
 
